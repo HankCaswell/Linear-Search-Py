272bc28c45e36825943dad9ddb174f22fcf38124
Hank Caswell <david.h.caswell@proton.me>
2024-02-01 18:13:36 -0500

    solved the challenge

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,18 @@
 def linear_search(value_to_find, array_to_search_through):
     # your code here
-    pass
+    for index, letter in enumerate(array_to_search_through):
+        if letter == value_to_find:
+            return index 
+     
+   
 
 def linear_search_global(value_to_find, array_to_search_through):
-    # your code here
-    pass
+    answer = []
+    for index, letter in enumerate(array_to_search_through):
+        if letter == value_to_find:
+            answer.append(index)
+    return answer 
+    
+    
+    
+
